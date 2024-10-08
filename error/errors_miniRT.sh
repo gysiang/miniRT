8b83250b2af2f0a5acf109e56d06f691ab147029
itben-h <67160296+itben-h@users.noreply.github.com>
2024-10-08 21:13:19 +0800

    more work

--- error/errors_miniRT.sh
+++ error/errors_miniRT.sh
@@ -5,16 +5,31 @@ COLOR_R='\033[0;31m'
 COLOR_G='\033[0;32m'
 COLOR_END='\033[0m'
 
-# Destination of miniRT
-MINIRT=../miniRT
+# Absolute path to miniRT (leave empty if in the same parent directory as the script)
+LOCATION=
+
+if test -z "$LOCATION" ; then
+	MINIRT="../miniRT"
+else
+	MINIRT="$LOCATION/miniRT"
+fi
+
+if test ! -f "$MINIRT" ; then
+	printf "${COLOR_R}Error: miniRT not found${COLOR_END}\n"
+	exit 1
+fi
 
 val_check () {
 	if grep -q "definitely lost:" val.log; then
-		printf "${COLOR_R}" && cat val.log && printf "${COLOR_END}"
+		printf "${COLOR_R}";
+		cat val.log;
+		printf "${COLOR_END}Stopping tests...\n";
+		rm val.log
+		exit 1
 	else
 		printf "[ ${COLOR_G}OK${COLOR_END} ]\n"
+		rm val.log
 	fi
-	rm -fr val.log
 }
 
 run_test () {
@@ -22,8 +37,20 @@ run_test () {
 	valgrind --leak-check=full --show-leak-kinds=all $MINIRT $2 2>val.log; val_check
 }
 
-# TESTS BELOW
+# SETUP
 
+# TESTS BELOW
 run_test "No file"
 run_test "Wrong file" a_file
+run_test "Non-existant file" nada.rt
+	touch nope.rt
+	chmod -r nope.rt
+run_test "Unreadable file" nope.rt
+	rm nope.rt
+run_test "Empty file" empty.rt
 run_test "Invalid ambient" A_extra.rt
+run_test "Invalid camera" C_extra.rt
+run_test "Invalid light" L_extra.rt
+run_test "Invalid plane" pl_extra.rt
+run_test "Invalid sphere" sp_extra.rt
+run_test "Invalid cylinder" cy_extra.rt
